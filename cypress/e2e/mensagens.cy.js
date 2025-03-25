d34e9181777866bb52abe1134657f81355c15403
Danillo Lopes <danillofll@gmail.com>
2025-03-25 12:18:19 -0300

    Envio de mensagem comunicação interna.

--- cypress/e2e/mensagens.cy.js
+++ cypress/e2e/mensagens.cy.js
@@ -38,21 +38,21 @@ describe("Mensagens", () => {
 
     //upload de Arquivo
     cy.get('input[type="file"]').attachFile(Foto_teste);
-    cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
     //apenas um click não está funcionando
     cy.get('.mt-0').click()
     cy.wait(1000)
     cy.get('.mt-0').click()
     
     cy.get(':nth-child(2) > button > .name > .row').click();
-        cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+        cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
 
     cy.contains(textocurto).click();
-        cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+        cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
 
     cy.contains(textolongo).should("be.visible");
     cy.contains(textolongo).should("be.visible");
-    cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
   });
 it('Envio Suporte pedagogico', () => {
     let textocurto = "Automação - " + faker.lorem.words(2);
@@ -69,20 +69,20 @@ it('Envio Suporte pedagogico', () => {
 
     //upload de Arquivo
     cy.get('input[type="file"]').attachFile(Foto_teste);
-    cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
     //apenas um click não está funcionando
     cy.get('.mt-0').click()
     cy.wait(1000)
     cy.get('.mt-0').click()
     cy.get(':nth-child(2) > button > .name > .row').click();
-        cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+        cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
 
     cy.contains(textocurto).click();
-        cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+        cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
 
     cy.contains(textolongo).should("be.visible");
     cy.contains(textolongo).should("be.visible");
-    cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
   });
   it('Envio Sugestão', () => {
     let textocurto = "Automação - " + faker.lorem.words(2);
@@ -99,56 +99,78 @@ it('Envio Suporte pedagogico', () => {
 
     //upload de Arquivo
     cy.get('input[type="file"]').attachFile(Foto_teste);
-    cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
     //apenas um click não está funcionando
     cy.get('.mt-0').click()
     cy.wait(1000)
     cy.get('.mt-0').click()
     cy.get(':nth-child(2) > button > .name > .row').click();
-        cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+        cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
 
     cy.contains(textocurto).click();
-        cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+        cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
 
     cy.contains(textolongo).should("be.visible");
     cy.contains(textolongo).should("be.visible");
-    cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
   });
 
-  it.only('Envio de Comunicação Interna', () => {
+  it('Envio de Comunicação Interna', () => {
+    let textocurto = "Automação - " + faker.lorem.words(2);
+    let textolongo = faker.lorem.paragraphs(1);
     cy.get(".header-menu > .br-button > .fas").click();
     cy.get(":nth-child(6) > #\\35 0").click();
     cy.get('[href="/conexao/mensagens/enviar"]:nth-child(1)').click();
     cy.get('#input_messages-type').click();
+    cy.wait(1000);
     cy.get(':nth-child(4) > .br-radio > label').click();
     cy.get('#state > .br-input > .br-button').click();
     cy.get('#state > .br-list > .w-100 > .br-checkbox > label').click();
     cy.get('#__next').click();
-    cy.get('#input_cities').click();
-    cy.get('#cities > .br-list > .w-100 > .br-checkbox > label').click();
+    cy.get('#input_cities', { timeout: 30000 }).click();
+    cy.wait(1000);
+    cy.get('#cities .w-100 label', { timeout: 30000 }).click();
+    cy.get('#__next').click();
+    cy.wait(1000);
+    cy.get('#input_institution').click();
+    cy.wait(1000);
+    cy.get('#institution > .br-list > .w-100 > .br-checkbox > label', { timeout: 30000 }).click();
+    cy.get('#__next').click();
+    cy.get('#input_categories').click();
+    cy.wait(1000);
+    cy.get('#categories > .br-list > .highlighted > .br-checkbox > label').click();
+    cy.get('#categories > .br-input > .br-button > .fas').click();
+    cy.get('.row > .br-button').click();
+    cy.wait(1000);
+    cy.get('#recipients > .br-input > .br-button > .fas').click();
+    cy.wait(1000);
+    cy.get('#recipients > .br-list > .w-100 > .br-checkbox > label').click();
+    cy.wait(1000);
+    cy.get('#recipients > .br-list > .w-100 > .br-checkbox > label').click();+
+    cy.get('.fa-angle-up:nth-child(2)').click();
+    cy.get('#subject-id1').type(textocurto);
+    cy.get('.se-wrapper-inner > p').click();
+    cy.get('.se-wrapper-inner > p').type(textolongo);
 
-    // cy.get('#subject-id1').type(textocurto);
-    // cy.get('.se-wrapper-inner > p').click();
-    // cy.get('.se-wrapper-inner > p').type(textolongo);
 
+    //upload de Arquivo
+    cy.get('input[type="file"]').attachFile(Foto_teste);
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
+    //apenas um click não está funcionando
 
-    // //upload de Arquivo
-    // cy.get('input[type="file"]').attachFile(Foto_teste);
-    // cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
-    // //apenas um click não está funcionando
-    // cy.get('.mt-0').click()
-    // cy.wait(1000)
-    // cy.get('.mt-0').click()
-    // cy.get(':nth-child(2) > button > .name > .row').click();
-    // cy.wait(5000)
-    //     cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+    cy.get('.false > .primary').click();
+    cy.wait(1000)
+    cy.get('.false > .primary').click();
+    cy.get(':nth-child(2) > button > .name > .row').click();
+    cy.wait(5000)
+        cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
 
-    // cy.contains(textocurto).click();
-    //     cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+    cy.contains(textocurto).click();
+        cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
 
-    // cy.contains(textolongo).should("be.visible");
-    // cy.contains(textolongo).should("be.visible");
-    // cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
+    cy.contains(textolongo).should("be.visible");
+    cy.contains(textolongo).should("be.visible");
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
   });
   it('Envio Reclamação', () => {
     let textocurto = "Automação - " + faker.lorem.words(2);
@@ -165,20 +187,20 @@ it('Envio Suporte pedagogico', () => {
 
     //upload de Arquivo
     cy.get('input[type="file"]').attachFile(Foto_teste);
-    cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
     //apenas um click não está funcionando
     cy.get('.mt-0').click()
     cy.wait(1000)
     cy.get('.mt-0').click()
     cy.get(':nth-child(2) > button > .name > .row').click();
-        cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+        cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
 
     cy.contains(textocurto).click();
-        cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+        cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
 
     cy.contains(textolongo).should("be.visible");
     cy.contains(textolongo).should("be.visible");
-    cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
   });
   it('Envio de Elogio', () => {
     let textocurto = "Automação - " + faker.lorem.words(2);
@@ -195,19 +217,19 @@ it('Envio Suporte pedagogico', () => {
 
     //upload de Arquivo
     cy.get('input[type="file"]').attachFile(Foto_teste);
-    cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
 
     //apenas um click não está funcionando
     cy.get('.mt-0').click()
     cy.wait(1000)
     cy.get('.mt-0').click()
     cy.get(':nth-child(2) > button > .name > .row').click();
-    cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+    cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
     cy.contains(textocurto).click();
-        cy.contains(textocurto, { timeout: 15000 }).should("be.visible");
+        cy.contains(textocurto, { timeout: 30000 }).should("be.visible");
 
     cy.contains(textolongo).should("be.visible");
     cy.contains(textolongo).should("be.visible");
-    cy.contains("Foto_teste.jpg", { timeout: 15000 }).should("be.visible");
+    cy.contains("Foto_teste.jpg", { timeout: 30000 }).should("be.visible");
   });
 });
