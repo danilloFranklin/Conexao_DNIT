b3bbef3d8f42a8e24f64150ec05dfc1064689f48
Danillo Lopes <danillofll@gmail.com>
2025-04-02 12:53:20 -0300

    Build inicial

--- cypress/e2e/Atividades.cy.js
+++ cypress/e2e/Atividades.cy.js
@@ -24,12 +24,12 @@ describe("Cursos", () => {
     cy.scrollTo(0, 456);
   });
 
-  it.only("Buscar atividade do EM", () => {
+  it("Buscar atividade do EM", () => {
     cy.get("#highSchool > #highSchool").should("be.visible").click();
+    cy.wait(1500);
     cy.get(".mt-4 .small:nth-child(1)").should("be.visible").click();
     cy.wait(1000)
-    cy.get(".mr-2").should("be.visible").click();
-    cy.scrollTo(0, 152);
-    cy.get(".mb-5:nth-child(4) > .br-button").should("be.visible").click();
+    cy.get('[style="font-size: 15px; font-weight: bold; background-color: rgb(200, 219, 255);"]').should("be.visible").click();
+    cy.get('#tabSent > form > .mb-5 > .br-button').should("be.visible").click();
   });
 });
