ce4603bd87015794e385c25252728e2068503edd
Danillo Lopes <danillofll@gmail.com>
2025-06-03 10:59:25 -0300

    Correção do USER para acesso ao gestão

--- cypress/e2e/Gestão/Cursos.cy.js
+++ cypress/e2e/Gestão/Cursos.cy.js
@@ -1,59 +1,17 @@
-import { faker } from "@faker-js/faker";
-
-describe("Atividades", () => {
-  const fileName = "home.pdf"; // Nome do arquivo esperado
-  const filePath = `cypress/downloads/${fileName}`;
-  const textoAleatorio = faker.lorem.sentence();
-
+describe("Paginas iniciais", () => {
   beforeEach(() => {
-    cy.visit("https://conexao-dnit-hom.labtrans.ufsc.br/conexao");
+    cy.visit("https://conexao-dnit-hom.labtrans.ufsc.br/conexao/"); // Acesse a página antes de setar o localStorage 
     cy.setLocalStorage();
+    // Recarrega a página para aplicar os valores do localStorage
     cy.reload();
 
-    // Verifica se a sessão e o usuário estão armazenados
+    // Valida se os dados foram inseridos corretamente
     cy.window().its("localStorage.session").should("exist");
     cy.window().its("localStorage.user").should("exist");
   });
-
-  it("Buscar atividades do EF", () => {
-    cy.get("#elementarySchool > #elementarySchool").should("be.visible").click();
-    cy.get(".wrapper > .br-button").should("be.visible").click();
-    cy.scrollTo(0, 77);
-    cy.get(".mb-3:nth-child(2) .br-button").should("be.visible").click();
-    cy.get(".mb-5:nth-child(4) > .br-button").should("be.visible").click();
-    cy.scrollTo(0, 456);
-    cy.get('div.text-primary-default')
-    .eq(0)
-    .trigger('click', { ctrlKey: false, metaKey: false });
-
-    cy.intercept('*').as('requests'); // Intercepta todas as requisições
-
-cy.get('div.text-primary-default').eq(0).click();
-
-cy.wait('@requests').then((interception) => {
-  cy.log('Requisição detectada:', interception.request.url);
-});
-
-    
-  });
-
-  it("Buscar atividade do EM", () => {
-    cy.get("#highSchool > #highSchool").should("be.visible").click();
-    cy.wait(1500);
-    cy.get(".mt-4 .small:nth-child(1)").should("be.visible").click();
-    cy.wait(1000)
-    cy.get('[style="font-size: 15px; font-weight: bold; background-color: rgb(200, 219, 255);"]').should("be.visible").click();
-    cy.get('#tabSent > form > .mb-5 > .br-button').should("be.visible").click();
-    cy.get('div.text-primary-default')
-    .eq(0)
-    .trigger('click', { ctrlKey: false, metaKey: false });
-
-    cy.intercept('*').as('requests'); // Intercepta todas as requisições
-
-cy.get('div.text-primary-default').eq(0).click();
-
-cy.wait('@requests').then((interception) => {
-  cy.log('Requisição detectada:', interception.request.url);
-});
-  });
-});
+  it("Sobre o Conexão", () => {
+    cy.visit("https://conexao-dnit-hom.labtrans.ufsc.br/conexao/gestao/");
+    cy.get('i.fa-bars').click();
+  }); 
+ });
+ 
